d64e56c997f41eb619f8f48f76ba4efe7f0c1110
alexandre thauvin <alexandre.thauvin@epitech.eu>
2017-03-09 01:47:49 +0100

    func think

--- src/action.c
+++ src/action.c
@@ -21,7 +21,7 @@ void      eat(t_philo *philo)
         philo->right->chopstick = true;
       }
 }
-/*
+
 void      think(t_philo *philo)
 {
   //trylock
@@ -32,7 +32,30 @@ void      think(t_philo *philo)
   //release
   //unlock
   //chopstick true
-}*/
+  //--
+  // meme processus avec celui de droite
+  if (pthread_mutex_trylock(&philo->mutex) && philo->right->state != THINK)
+    {
+      philo->chopstick = false;
+      philo->state = THINK;
+      lphilo_take_chopstick(&philo->mutex);
+      lphilo_think();
+      lphilo_release_chopstick(&philo->mutex);
+      pthread_mutex_unlock(&philo->mutex);
+      philo->chopstick = true;
+    }
+  else if (pthread_mutex_trylock(&philo->right->mutex)
+	   && philo->right->state != THINK)
+    {
+      philo->chopstick = false;
+      philo->state = THINK;
+      lphilo_take_chopstick(&philo->right->mutex);
+      lphilo_think();
+      lphilo_release_chopstick(&philo->right->mutex);
+      pthread_mutex_unlock(&philo->right->mutex);
+      philo->chopstick = true;
+    }
+}
 
 void      rest(t_philo *philo)
 {
